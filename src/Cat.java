66268d5b7fc3e30dbbef33f6c35f2775f679b07d
Truyen Van <tvan@mymail.mines.edu>
2016-09-27 15:00:53 -0600

    Made head, eyes, and mouth smaller, made mouth pink

--- src/Cat.java
+++ src/Cat.java
@@ -5,16 +5,16 @@ import java.awt.geom.*;
 public class Cat {
 	// drawing constants are private - noone needs to know what we're doing
 	// pick a head dimension
-	private static final int HEAD_DIMENSION = 350;
+	private static final int HEAD_DIMENSION = 10;
 	// eyes will be about 1/4 from top of head and 1/4 from left
 	private static final int EYE_Y = HEAD_DIMENSION/4;
 	private static final int EYE_X = HEAD_DIMENSION/4;
 	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
 	// pick eye dimensions
-	private static final int EYE_HEIGHT = 100;
-	private static final int EYE_WIDTH = 50;
+	private static final int EYE_HEIGHT = 5;
+	private static final int EYE_WIDTH = 2;
 	// pick mouth height, width is based on head dimension
-	private static final int MOUTH_HEIGHT = 60;
+	private static final int MOUTH_HEIGHT = 6;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
@@ -37,7 +37,7 @@ public class Cat {
 		x += EYE_SEPARATION;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
-		g2.setColor(Color.green);
+		g2.setColor(Color.pink);
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
