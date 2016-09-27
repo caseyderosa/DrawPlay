ebe9472e0e69c277149b68c749c2127726f4eaba
Casey DeRosa <cderosa@mymail.mines.edu>
2016-09-27 14:56:13 -0600

    Made cat head bigger

--- src/Cat.java
+++ src/Cat.java
@@ -5,7 +5,7 @@ import java.awt.geom.*;
 public class Cat {
 	// drawing constants are private - noone needs to know what we're doing
 	// pick a head dimension
-	private static final int HEAD_DIMENSION = 150;
+	private static final int HEAD_DIMENSION = 250;
 	// eyes will be about 1/4 from top of head and 1/4 from left
 	private static final int EYE_Y = HEAD_DIMENSION/4;
 	private static final int EYE_X = HEAD_DIMENSION/4;
@@ -37,13 +37,13 @@ public class Cat {
 		x += EYE_SEPARATION;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
-		g2.setColor(Color.yellow);
+		g2.setColor(Color.green);
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Oh hi", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("Stupid Cat", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
